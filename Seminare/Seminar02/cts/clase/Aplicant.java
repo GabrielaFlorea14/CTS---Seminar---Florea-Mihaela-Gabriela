d40dbf7941c19db7cf61484c60e6a8e376b8c8b1
Florea <floreamihaela20@stud.ase.ro>
2023-03-06 09:34:47 +0200

    seminar2

---
+++ Seminare/Seminar02/cts/clase/Aplicant.java
@@ -0,0 +1,66 @@
+package cts.clase;
+
+public abstract class Aplicant{
+	protected String nume;
+	protected String prenume;
+	protected int varsta;
+	protected int punctaj;
+	protected int nrProiecte;
+	protected String[] denumiriProiect;
+	
+	
+	public String getNume() {
+		return nume;
+	}
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public String getPrenume() {
+		return prenume;
+	}
+	public void setPrenume(String prenume) {
+		this.prenume = prenume;
+	}
+	public int getVarsta() {
+		return varsta;
+	}
+	public void setVarsta(int varsta) {
+		this.varsta = varsta;
+	}
+	public void statut(){
+		if(punctaj>80)
+			System.out.println("Aplicantul "+nume+" "+prenume+" a fost acceptat.");
+		else
+			System.out.println("Aplicantul "+nume+" "+prenume+" nu a fost acceptat.");
+		}
+	public int getPunctaj() {
+		return punctaj;
+	}
+	public void setPunctaj(int punctaj) {
+		this.punctaj = punctaj;
+	}
+	
+	
+
+	
+	public Aplicant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Aplicant(String nume, String prenume, int varsta, int punctaj, int nrProiecte, String[] denumiriProiect) {
+		super();
+		this.nume = nume;
+		this.prenume = prenume;
+		this.varsta = varsta;
+		this.punctaj = punctaj;
+		this.nrProiecte = nrProiecte;
+		this.denumiriProiect = denumiriProiect;
+	}
+	public int getnrProiecte() {
+		return nrProiecte;
+	}
+	public void setnrProiecte(int nrProiecte) {
+		this.nrProiecte = nrProiecte;
+	}
+
+}
